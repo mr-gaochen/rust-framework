dfa47138cf926065a62569c3942fa521095abc48
eric <mr.xinyi@yahoo.com>
2024-08-29 10:21:53 +0800

    update

--- src/service/generic_service.rs
+++ src/service/generic_service.rs
@@ -1,7 +1,9 @@
 use async_trait::async_trait;
-use sea_orm::{DatabaseConnection, DbErr, DeleteResult, EntityTrait, PrimaryKeyTrait};
+use sea_orm::{
+    sea_query::IntoCondition, DatabaseConnection, DbErr, DeleteResult, EntityTrait, PrimaryKeyTrait,
+};
 
-use crate::repo::repo::Repo;
+use crate::{dto::request::PageQueryParam, repo::repo::Repo};
 
 use super::service::Service;
 
@@ -42,6 +44,55 @@ where
         self.dao.find_by_id(db, id).await
     }
 
+    async fn find_one_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+    ) -> Result<Option<E::Model>, DbErr>
+    where
+        F: IntoCondition + Send,
+    {
+        self.dao.find_one_condition(db, filter).await
+    }
+
+    // 集合查询全量列表
+    async fn find_list(&self, db: &DatabaseConnection) -> Result<Vec<E::Model>, DbErr> {
+        self.dao.find_list(db).await
+    }
+
+    // 集合条件查询列表
+    async fn find_by_list_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+    ) -> Result<Vec<E::Model>, DbErr>
+    where
+        F: IntoCondition + Send,
+    {
+        self.dao.find_by_list_condition(db, filter).await
+    }
+
+    async fn find_page(
+        &self,
+        db: &DatabaseConnection,
+        param: &PageQueryParam,
+    ) -> Result<(Vec<E::Model>, u64), DbErr> {
+        self.dao.find_page(db, param).await
+    }
+
+    // 分页条件查询
+    async fn find_page_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+        param: &PageQueryParam,
+    ) -> Result<(Vec<E::Model>, u64), DbErr>
+    where
+        F: IntoCondition + Send,
+    {
+        self.dao.find_page_condition(db, filter, param).await
+    }
+
     async fn create(&self, db: &DatabaseConnection, model: E::Model) -> Result<E::Model, DbErr> {
         self.dao.create(db, model).await
     }
@@ -50,7 +101,30 @@ where
         self.dao.update(db, model).await
     }
 
+    async fn update_by_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+        model: E::Model,
+    ) -> Result<u64, DbErr>
+    where
+        F: IntoCondition + Send,
+    {
+        self.dao.update_by_condition(db, filter, model).await
+    }
+
     async fn delete(&self, db: &DatabaseConnection, id: Pk) -> Result<DeleteResult, DbErr> {
         self.dao.delete(db, id).await
+    }
+
+    async fn delete_batch<C>(
+        &self,
+        db: &DatabaseConnection,
+        condition: C,
+    ) -> Result<DeleteResult, DbErr>
+    where
+        C: IntoCondition + Send,
+    {
+        self.dao.delete_batch(db, condition).await
     }
 }
--- src/service/service.rs
+++ src/service/service.rs
@@ -1,5 +1,9 @@
 use async_trait::async_trait;
-use sea_orm::{DatabaseConnection, DbErr, DeleteResult, EntityTrait, PrimaryKeyTrait};
+use sea_orm::{
+    sea_query::IntoCondition, DatabaseConnection, DbErr, DeleteResult, EntityTrait, PrimaryKeyTrait,
+};
+
+use crate::dto::request::PageQueryParam;
 
 // 定义 Service Trait，泛型 E 是 Entity 类型，Pk 是主键类型
 #[async_trait]
@@ -11,12 +15,67 @@ where
     // 查找某个实体
     async fn find_by_id(&self, db: &DatabaseConnection, id: Pk) -> Result<Option<E::Model>, DbErr>;
 
+    // 条件查询某个实体
+    async fn find_one_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+    ) -> Result<Option<E::Model>, DbErr>
+    where
+        F: IntoCondition + Send;
+
+    // 集合查询全量列表
+    async fn find_list(&self, db: &DatabaseConnection) -> Result<Vec<E::Model>, DbErr>;
+
+    // 集合条件查询列表
+    async fn find_by_list_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+    ) -> Result<Vec<E::Model>, DbErr>
+    where
+        F: IntoCondition + Send;
+
+    async fn find_page(
+        &self,
+        db: &DatabaseConnection,
+        param: &PageQueryParam,
+    ) -> Result<(Vec<E::Model>, u64), DbErr>;
+
+    // 分页条件查询
+    async fn find_page_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+        param: &PageQueryParam,
+    ) -> Result<(Vec<E::Model>, u64), DbErr>
+    where
+        F: IntoCondition + Send;
+
     // 创建新实体
     async fn create(&self, db: &DatabaseConnection, model: E::Model) -> Result<E::Model, DbErr>;
 
     // 更新实体
     async fn update(&self, db: &DatabaseConnection, model: E::Model) -> Result<E::Model, DbErr>;
 
+    // 条件更新
+    async fn update_by_condition<F>(
+        &self,
+        db: &DatabaseConnection,
+        filter: F,
+        model: E::Model,
+    ) -> Result<u64, DbErr>
+    where
+        F: IntoCondition + Send;
+
     // 删除实体
     async fn delete(&self, db: &DatabaseConnection, id: Pk) -> Result<DeleteResult, DbErr>;
+
+    async fn delete_batch<C>(
+        &self,
+        db: &DatabaseConnection,
+        condition: C,
+    ) -> Result<DeleteResult, DbErr>
+    where
+        C: IntoCondition + Send;
 }
